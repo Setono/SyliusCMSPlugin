eadc585d339cf07c9f8e52a5ae72bd7e26a4109c
Joachim Løvgaard <joachim@loevgaard.dk>
2021-10-01 08:46:47 +0200

    Import

--- src/Checker/Eligibility/Page/EnabledDateEligibilityChecker.php
+++ src/Checker/Eligibility/Page/EnabledDateEligibilityChecker.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Setono\SyliusCMSPlugin\Checker\Eligibility\Page;
 
+use DateTimeImmutable;
 use Setono\SyliusCMSPlugin\Model\PageInterface;
 
 final class EnabledDateEligibilityChecker implements EligibilityCheckerInterface
 {
     public function isEligible(PageInterface $page): bool
     {
-        return $page->isEnabledOn(new \DateTimeImmutable());
+        return $page->isEnabledOn(new DateTimeImmutable());
     }
 }
--- src/DependencyInjection/Compiler/RegisterPageEligibilityCheckersPass.php
+++ src/DependencyInjection/Compiler/RegisterPageEligibilityCheckersPass.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\SyliusCMSPlugin\DependencyInjection\Compiler;
 
+use function array_keys;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
