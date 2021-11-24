48e256617995d18159b566e6f2955de57229440d
Roshyo <stefdecock68@gmail.com>
2021-11-24 13:01:57 +0100

    Add the possibility to change the block position

--- src/Form/Type/CarouselBlockType.php
+++ src/Form/Type/CarouselBlockType.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Setono\SyliusCMSPlugin\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 final class CarouselBlockType extends AbstractResourceType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('block', BlockAutocompleteChoiceType::class);
+        $builder->add('block', BlockAutocompleteChoiceType::class, ['label' => false]);
+        $builder->add('position', IntegerType::class, ['label' => 'sylius.ui.position']);
     }
 }
