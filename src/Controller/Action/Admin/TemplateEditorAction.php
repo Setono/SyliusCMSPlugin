2fda8d896bef08f1a9f3b80c60ed969b0874cb3e
Joachim Løvgaard <joachim@loevgaard.dk>
2022-06-28 08:14:41 +0200

    Add monaco editor for template editing

---
+++ src/Controller/Action/Admin/TemplateEditorAction.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusCMSPlugin\Controller\Action\Admin;
+
+use Setono\SyliusCMSPlugin\Repository\TemplateRepositoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class TemplateEditorAction
+{
+    private TemplateRepositoryInterface $templateRepository;
+
+    private Environment $twig;
+
+    public function __construct(TemplateRepositoryInterface $templateRepository, Environment $twig)
+    {
+        $this->templateRepository = $templateRepository;
+        $this->twig = $twig;
+    }
+
+    public function __invoke(Request $request, int $id = null): Response
+    {
+        $content = null;
+        if (null !== $id) {
+            $template = $this->templateRepository->find($id);
+            if (null !== $template) {
+                $content = $template->getSource();
+            }
+        }
+
+        return new Response($this->twig->render('@SetonoSyliusCMSPlugin/admin/template/editor.html.twig', [
+            'content' => $content,
+            'formElement' => $request->query->get('formElement'),
+        ]));
+    }
+}
--- src/Form/Type/TemplateType.php
+++ src/Form/Type/TemplateType.php
@@ -6,6 +6,7 @@ namespace Setono\SyliusCMSPlugin\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -14,7 +15,7 @@ final class TemplateType extends AbstractResourceType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('source', TextareaType::class, [
+            ->add('source', HiddenType::class, [
                 'label' => 'setono_sylius_cms.form.template.source',
             ])
             ->add('internalDescription', TextareaType::class, [
