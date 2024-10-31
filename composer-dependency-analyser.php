9827925b8ca953d1589d7e06d5539b5ac7bcee8f
Joachim Løvgaard <joachim@loevgaard.dk>
2024-10-31 09:18:36 +0100

    Fixing dep analysis

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,14 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('knplabs/knp-gaufrette-bundle', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/cache', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('setono/editorjs-bundle', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('setono/editorjs-php', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('stof/doctrine-extensions-bundle', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/property-access', [ErrorType::UNUSED_DEPENDENCY])
 ;
